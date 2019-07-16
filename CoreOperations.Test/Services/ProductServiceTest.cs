08662fa507c6611d756e3ace64bd7bcf5974e104
umeshkumarswain <umeshkumarswain5@gmail.com>
2019-07-16 22:56:42 +0530

    Unit Testing Project Added

---
+++ CoreOperations.Test/Services/ProductServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using CoreOperations.Models;
+using CoreOperations.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreOperations.Test.Services
+{
+    [TestClass]
+    public class ProductServiceTest
+    {
+
+        Mock<IProductService> mockProductService = new Mock<IProductService>();
+        [TestMethod]
+        public void GetAll()
+        {
+            var products = new List<Product>() {
+                new Product{ Name="Product One",Color="Green",ListPrice=12}
+            };
+            var productservice = new Mock<IProductService>();
+            var result = productservice.Setup( item => item.GetAllProducts())
+                           .Returns(products);
+            Assert.AreEqual(products, result);
+
+
+        }
+    }
+}
