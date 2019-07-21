84dfaf5b98101adc51cdf921cd5c089f4c4064c6
Umesh Kumar <umeshkumarswain5@outlook.com>
2019-07-21 08:02:40 +0530

    Application Insights Configured for CoreMVC Project

--- CoreClient/Program.cs
+++ CoreClient/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
